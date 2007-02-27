6e49bf2fcd21cb48c03122a7277ee1161fbe8fb6
jorton <jorton@fedoraproject.org>
2007-02-27 17:45:50 +0000

    - filter more Apache::Test requirements

--- filter-requires.sh
+++ filter-requires.sh
@@ -3,7 +3,6 @@
 /usr/lib/rpm/perl.req $* |\
     sed -e '/perl(Apache2::FunctionTable)/d' \
         -e '/perl(Apache2::StructureTable)/d' \
-        -e '/perl(Apache::TestConfigParse)/d' \
-        -e '/perl(Apache::TestConfigPerl)/d' \
+        -e '/perl(Apache::Test.*)/d' \
 	-e '/perl(Data::Flow)/d' \
 	-e '/perl(Module::Build)/d' 
