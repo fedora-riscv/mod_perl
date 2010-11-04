8de8ea158ec4ac8e28b98308d3c8d3d2dd122b8e
Marcela Mašláňová <mmaslano@redhat.com>
2010-11-04 17:17:09 +0100

    Commit Emmanuel Seyman merge review changes.

--- filter-requires.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-/usr/lib/rpm/perl.req $* |\
-    sed -e '/perl(Apache2::FunctionTable)/d' \
-        -e '/perl(Apache2::StructureTable)/d' \
-        -e '/perl(Apache::Test.*)/d' \
-	-e '/perl(Data::Flow)/d' \
-	-e '/perl(Module::Build)/d' 
